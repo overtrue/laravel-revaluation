77152edfc3838b056cd0814ee576f404cf0cb077
安正超 <anzhengchao@gmail.com>
2017-07-05 19:27:10 +0800

    Apply fixes from StyleCI (#3)

--- tests/stubs/migrations/2017_01_09_132402_create_orders_table.php
+++ tests/stubs/migrations/2017_01_09_132402_create_orders_table.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the overtrue/laravel-revaluation.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -8,8 +17,6 @@ class CreateOrdersTable extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
@@ -32,8 +39,6 @@ class CreateOrdersTable extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down()
     {
